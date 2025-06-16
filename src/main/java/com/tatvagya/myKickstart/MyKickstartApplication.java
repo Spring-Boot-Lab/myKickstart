7341af2a3859813e61900b8243de3ada4dcf0f36
Tatvagya Nahar <tatvagyanahar@gmail.com>
2025-06-16 14:18:04 +0530

    Committed myKickstart project

---
+++ src/main/java/com/tatvagya/myKickstart/MyKickstartApplication.java
@@ -0,0 +1,13 @@
+package com.tatvagya.myKickstart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyKickstartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyKickstartApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tatvagya/myKickstart/MyKickstartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tatvagya.myKickstart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyKickstartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
